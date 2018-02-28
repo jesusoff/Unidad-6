9e68004915a36eec839a42170c83f25d1fd8e3dc
jesus <jesuscastillo0787@gmail.com>
2018-02-28 21:54:31 +0100

    caso practico 1 actualizado

--- Caso practico 1 unidad 6/conversion.php
+++ Caso practico 1 unidad 6/conversion.php
@@ -6,10 +6,11 @@
   </head>
   <body>
     <?php
+    //separo los colores y los guardo en variables
     $rojo = substr($_POST["hexadecimal"], 1, 2);
     $verde = substr($_POST["hexadecimal"], 3, 2);
     $azul = substr($_POST["hexadecimal"], 5, 2);
-
+    //imprimo por pantalla los colores en RGB
     echo "El color hexadecimal ".$_POST["hexadecimal"]." en RGB es:<br>";
     echo "Rojo: ".hexdec($rojo)."<br>";
     echo "Verde: ".hexdec($verde)."<br>";
